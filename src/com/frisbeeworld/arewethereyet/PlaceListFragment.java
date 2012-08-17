e8215dba22a98c2415584d3106a8a8a48719e1e8
Derek Waters <derek@frisbeeworld.com>
2012-08-17 20:59:09 +1000

    Icon and Layout changes

--- src/com/frisbeeworld/arewethereyet/PlaceListFragment.java
+++ src/com/frisbeeworld/arewethereyet/PlaceListFragment.java
@@ -1,12 +1,8 @@
 package com.frisbeeworld.arewethereyet;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
