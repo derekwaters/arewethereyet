7531f1039d3aaf6b11f26b43f6a96634d9cd0dab
Derek Waters <derek@frisbeeworld.com>
2012-08-28 22:04:38 +1000

    Added Layout Changes for the Add Place activity

--- src/com/frisbeeworld/arewethereyet/AddPlaceActivity.java
+++ src/com/frisbeeworld/arewethereyet/AddPlaceActivity.java
@@ -1,8 +1,12 @@
 package com.frisbeeworld.arewethereyet;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AddPlaceActivity extends Activity {
 
@@ -10,8 +14,46 @@ public class AddPlaceActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+        
+        
+        
+     // Inflate a "Done/Discard" custom action bar view.
+        LayoutInflater inflater = (LayoutInflater) getActionBar().getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        // LayoutInflater inflater = (LayoutInflater)this.getBaseContext().
+        // 		getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View customActionBarView = inflater.inflate(
+                R.layout.actionbar_done_discard, null);
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Done"
+                        finish(); // TODO: don't just finish()!
+                    }
+                });
+        customActionBarView.findViewById(R.id.actionbar_discard).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Discard"
+                        finish(); // TODO: don't just finish()!
+                    }
+                });
+
+        // Show the custom action bar view and hide the normal Home icon and title.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+                        | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView, new ActionBar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
     }
 
+        
+     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_add_place, menu);
